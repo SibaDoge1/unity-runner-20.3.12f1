33eefd8e67cfe7b0aa2a541ac43027d46e52b968
SibaDoge <38933716+SibaDoge1@users.noreply.github.com>
2021-07-07 02:59:46 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ pwd
 ls -l
 
 xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
-/opt/Unity/Editor/Unity \
+/opt/unity/Editor/Unity \
   -batchmode \
   -nographics \
   -logfile /dev/stdout \
