2bee8cce72dd98a41ea665568b409a3b0f50faf4
SibaDoge <38933716+SibaDoge1@users.noreply.github.com>
2021-07-07 02:58:56 +0900

    Update request_activation.sh

--- request_activation.sh
+++ request_activation.sh
@@ -2,7 +2,7 @@
 
 # Request the manual activation file for activating unity personal
 xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
-  /opt/Unity/Editor/Unity \
+  /opt/unity/Editor/Unity \
     -batchmode \
     -nographics \
     -logFile /dev/stdout \
