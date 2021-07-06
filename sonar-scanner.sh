fa3082869c182d8453b9cb71384c90d3d1fc9544
SibaDoge <38933716+SibaDoge1@users.noreply.github.com>
2021-07-07 03:00:07 +0900

    Update sonar-scanner.sh

--- sonar-scanner.sh
+++ sonar-scanner.sh
@@ -43,6 +43,6 @@ fi
 
 sh -c "$begin_cmd"
 
-sh -c "/opt/Unity/Editor/Data/NetCore/Sdk-2.2.107/dotnet build ${INPUT_SOLUTION}"
+sh -c "/opt/unity/Editor/Data/NetCore/Sdk-2.2.107/dotnet build ${INPUT_SOLUTION}"
 
 sh -c "$end_cmd"
